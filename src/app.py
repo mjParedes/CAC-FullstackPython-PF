9f5fa25980600a5f0dfb2a2715acdd03c16c5a4b
Julian Rojas <rojasjulianf96@gmail.com>
2023-07-01 15:39:33 +0200

    pasando a etapa 3

--- src/app.py
+++ src/app.py
@@ -1,10 +1,47 @@
+import sqlite3
+
+
+# Configurar la conexión a la base de datos SQLite
+DATABASE = 'inventario.db'
+
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+# Crear la tabla 'productos' si no existe
+def create_table():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS productos (
+            codigo INTEGER PRIMARY KEY,
+            descripcion TEXT NOT NULL,
+            cantidad INTEGER NOT NULL,
+            precio REAL NOT NULL
+        ) ''')
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+# Verificar si la base de datos existe, si no, crearla y crear la tabla
+def create_database():
+    conn = sqlite3.connect(DATABASE)
+    conn.close()
+    create_table()
+
+
 class Producto:
     # Definimos el constructor e inicializamos los atributos de instancia
     def __init__(self, codigo, descripcion, cantidad, precio):
-        self.codigo = codigo           # Código
-        self.descripcion = descripcion  # Descripción
+        self.codigo = codigo           # Código 
+        self.descripcion = descripcion # Descripción
         self.cantidad = cantidad       # Cantidad disponible (stock)
-        self.precio = precio           # Precio
+        self.precio = precio           # Precio 
+
 
     # Este método permite modificar un producto.
     def modificar(self, nueva_descripcion, nueva_cantidad, nuevo_precio):
@@ -13,33 +50,84 @@ class Producto:
         self.precio = nuevo_precio            # Modifica el precio
 
 
+    def agregar_producto(self, codigo, descripcion, cantidad, precio):
+        producto_existente = self.consultar_producto(codigo)
+        if producto_existente:
+            print("Ya existe un producto con ese código.")
+            return False
+        nuevo_producto = Producto(codigo, descripcion, cantidad, precio)
+        sql = f'INSERT INTO productos VALUES ({codigo}, "{descripcion}", {cantidad}, {precio});'
+        self.cursor.execute(sql)
+        self.conexion.commit()
+        return True
+    
+    def consultar_producto(self, codigo):
+        sql = f'SELECT * FROM productos WHERE codigo = {codigo};'
+        self.cursor.execute(sql)
+        row = self.cursor.fetchone()
+        if row:
+            codigo, descripcion, cantidad, precio = row
+            return Producto(codigo, descripcion, cantidad, precio)
+        return False
+    
+    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio):
+        producto = self.consultar_producto(codigo)
+        if producto:
+            producto.modificar(nueva_descripcion, nueva_cantidad, nuevo_precio)
+            sql = f'UPDATE productos SET descripcion = "{nueva_descripcion}", cantidad = {nueva_cantidad}, precio = {nuevo_precio} WHERE codigo = {codigo};' 
+            self.cursor.execute(sql)
+            self.conexion.commit()
+            
+    def eliminar_producto(self, codigo):
+        sql = f'DELETE FROM productos WHERE codigo = {codigo};' 
+        self.cursor.execute(sql)
+        if self.cursor.rowcount > 0:
+            print(f'Producto {codigo} eliminado.')
+            self.conexion.commit()
+        else:
+            print(f'Producto {codigo} no encontrado.')
+
+    def listar_productos(self):
+        print("-"*50)
+        print("Lista de productos en el inventario:")
+        print("Código\tDescripción\tCant\tPrecio")
+        self.cursor.execute("SELECT * FROM productos")
+        rows = self.cursor.fetchall()
+        for row in rows:
+            codigo, descripcion, cantidad, precio = row
+            print(f'{codigo}\t{descripcion}\t{cantidad}\t{precio}')
+        print("-"*50)
+
+#INVENTARIO
+
 class Inventario:
     # Definimos el constructor e inicializamos los atributos de instancia
     def __init__(self):
-        # Lista de productos en el inventario (variable de clase)
-        self.productos = []
+        self.productos = []  # Lista de productos en el inventario (variable de clase)
+
 
     # Este método permite crear objetos de la clase "Producto" y agregarlos al inventario.
     def agregar_producto(self, codigo, descripcion, cantidad, precio):
         nuevo_producto = Producto(codigo, descripcion, cantidad, precio)
-        # Agrega un nuevo producto a la lista
-        self.productos.append(nuevo_producto)
+        self.productos.append(nuevo_producto)  # Agrega un nuevo producto a la lista
+
 
     # Este método permite consultar datos de productos que están en el inventario
     # Devuelve el producto correspondiente al código proporcionado o False si no existe.
     def consultar_producto(self, codigo):
         for producto in self.productos:
             if producto.codigo == codigo:
-                return producto  # Retorna un objeto
+                return producto # Retorna un objeto
         return False
 
-    # Este método permite modificar datos de productos que están en el inventario
 
+    # Este método permite modificar datos de productos que están en el inventario
     # Utiliza el método consultar_producto del inventario y modificar del producto.
     def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio):
         producto = self.consultar_producto(codigo)
         if producto:
             producto.modificar(nueva_descripcion, nueva_cantidad, nuevo_precio)
+
 
     # Este método elimina el producto indicado por codigo de la lista mantenida en el inventario.
     def eliminar_producto(self, codigo):
@@ -47,12 +135,13 @@ class Inventario:
         for producto in self.productos:
             if producto.codigo == codigo:
                 eliminar = True
-                producto_eliminar = producto
+                producto_eliminar = producto       
         if eliminar == True:
             self.productos.remove(producto_eliminar)
             print(f'Producto {codigo} eliminado.')
         else:
             print(f'Producto {codigo} no encontrado.')
+
 
     # Este método imprime en la terminal una lista con los datos de los productos que figuran en el inventario.
     def listar_productos(self):
@@ -60,50 +149,47 @@ class Inventario:
         print("Lista de productos en el inventario:")
         print("Código\tDescripción\t\tCant\tPrecio")
         for producto in self.productos:
-            print(
-                f'{producto.codigo}\t{producto.descripcion}\t{producto.cantidad}\t{producto.precio}')
+            print(f'{producto.codigo}\t{producto.descripcion}\t{producto.cantidad}\t{producto.precio}')
         print("-"*50)
 
 
+#CARRITO
 class Carrito:
-    # Definimos el constructor e inicializamos los atributos de instancia
-    def __init__(self):
-        self.items = []  # Lista de items en el carrito (variable de clase)
 
-    # Este método permite agregar productos del inventario al carrito.
+    def __init__(self):
+        self.conexion = get_db_connection()
+        self.cursor = self.conexion.cursor()
+        self.items = []
+
+
     def agregar(self, codigo, cantidad, inventario):
-        # Nos aseguramos que el producto esté en el inventario
         producto = inventario.consultar_producto(codigo)
         if producto is False:
             print("El producto no existe.")
             return False
-
-        # Verificamos que la cantidad en stock sea suficiente
         if producto.cantidad < cantidad:
             print("Cantidad en stock insuficiente.")
             return False
 
-        # Si existe y hay stock, vemos si ya existe en el carrito.
+
         for item in self.items:
             if item.codigo == codigo:
                 item.cantidad += cantidad
-                # Actualizamos la cantidad en el inventario
-                producto = inventario.consultar_producto(codigo)
-                producto.modificar(
-                    producto.descripcion, producto.cantidad - cantidad, producto.precio)
+                sql = f'UPDATE productos SET cantidad = cantidad - {cantidad}  WHERE codigo = {codigo};'
+                self.cursor.execute(sql)
+                self.conexion.commit()
                 return True
 
-        # Si no existe en el carrito, lo agregamos como un nuevo item.
-        nuevo_item = Producto(codigo, producto.descripcion,
-                              cantidad, producto.precio)
+
+        nuevo_item = Producto(codigo, producto.descripcion, cantidad, producto.precio)
         self.items.append(nuevo_item)
-        # Actualizamos la cantidad en el inventario
-        producto = inventario.consultar_producto(codigo)
-        producto.modificar(producto.descripcion,
-                           producto.cantidad - cantidad, producto.precio)
+        sql = f'UPDATE productos SET cantidad = cantidad - {cantidad}  WHERE codigo = {codigo};'
+        self.cursor.execute(sql)
+        self.conexion.commit()
         return True
 
-    # Este método quita unidades de un elemento del carrito, o lo elimina.
+
+
     def quitar(self, codigo, cantidad, inventario):
         for item in self.items:
             if item.codigo == codigo:
@@ -113,97 +199,43 @@ class Carrito:
                 item.cantidad -= cantidad
                 if item.cantidad == 0:
                     self.items.remove(item)
-                # Actualizamos la cantidad en el inventario
-                producto = inventario.consultar_producto(codigo)
-                producto.modificar(
-                    producto.descripcion, producto.cantidad + cantidad, producto.precio)
+                sql = f'UPDATE productos SET cantidad = cantidad + {cantidad} WHERE codigo = {codigo};'
+                self.cursor.execute(sql)
+                self.conexion.commit()
                 return True
-
-        # Si el bucle finaliza sin novedad, es que ese producto NO ESTA en el carrito.
         print("El producto no se encuentra en el carrito.")
         return False
+
 
     def mostrar(self):
         print("-"*50)
         print("Lista de productos en el carrito:")
-        print("Código\tDescripción\t\tCant\tPrecio")
+        print("Código\tDescripción\tCant\tPrecio")
         for item in self.items:
-            print(
-                f'{item.codigo}\t{item.descripcion}\t{item.cantidad}\t{item.precio}')
+            print(f'{item.codigo}\t{item.descripcion}\t{item.cantidad}\t{item.precio}')
         print("-"*50)
 
 
-'''Programa principal
-producto = Producto(1, 'Teclado USB 101 teclas', 10, 4500)
-# Accedemos a los atributos del objeto
-print(f'{producto.codigo} | {producto.descripcion} | {producto.cantidad} | {producto.precio}')
-# Modificar los datos del producto
-producto.modificar('Teclado Mecánico USB', 20, 4800)
-print(f'{producto.codigo} | {producto.descripcion} | {producto.cantidad} | {producto.precio}')
-'''
+
+# Programa principal
+# Crear la base de datos y la tabla si no existen
+create_database()
+
 
 # Crear una instancia de la clase Inventario
 mi_inventario = Inventario()
 
-# Agregar productos
-mi_inventario.agregar_producto(1, 'Teclado USB 101 teclas', 10, 4500)
-mi_inventario.agregar_producto(2, 'Mouse USB 3 botones', 5, 2500)
-mi_inventario.agregar_producto(3, 'Monitor LCD 22 pulgadas', 15, 52500)
-mi_inventario.agregar_producto(4, 'Monitor LCD 27 pulgadas', 25, 78500)
-mi_inventario.agregar_producto(5, 'Mouse Pad color azul', 5, 500)
 
-# Consultar un producto
-producto = mi_inventario.consultar_producto(3)
-if producto != False:
-    print(
-        f'Producto encontrado:\nCódigo: {producto.codigo}\nDescripción: {producto.descripcion}\nCantidad: {producto.cantidad}\nPrecio: {producto.precio}')
-else:
-    print("Producto no encontrado.")
+# Agregar productos al inventario
+mi_inventario.agregar_producto(1, "Producto 1", 10, 19.99)
+mi_inventario.agregar_producto(2, "Producto 2", 5, 9.99)
+mi_inventario.agregar_producto(3, "Producto 3", 15, 29.99)
 
-# Modificar un producto
-mi_inventario.modificar_producto(3, 'Monitor LCD 24 pulgadas', 5, 62000)
 
-# Listar todos los productos
-mi_inventario.listar_productos()
+# Consultar algún producto del inventario
+print(mi_inventario.consultar_producto(3)) #Existe, se muestra la dirección de memoria
+print(mi_inventario.consultar_producto(4)) #No existe, se muestra False
 
-# Eliminar un producto
-mi_inventario.eliminar_producto(2)
 
-# Confirmamos que haya sido eliminado
-mi_inventario.listar_productos()
-
-# ---------------------------------------------------------------------
-# Ejemplo de uso de las clases y objetos definidos antes:
-# ---------------------------------------------------------------------
-
-# Crear una instancia de la clase Inventario
-mi_inventario = Inventario()
-
-# Crear una instancia de la clase Carrito
-mi_carrito = Carrito()
-
-# Crear 3 productos y agregarlos al inventario
-mi_inventario.agregar_producto(1, 'Teclado USB 101 teclas', 10, 4500)
-mi_inventario.agregar_producto(2, 'Mouse USB 3 botones', 5, 2500)
-mi_inventario.agregar_producto(3, 'Monitor LCD 22 pulgadas', 15, 52500)
-
-# Listar todos los productos del inventario
-mi_inventario.listar_productos()
-
-# Agregar 2 productos al carrito
-# Agregar 2 unidades del producto con código 1 al carrito
-mi_carrito.agregar(1, 2, mi_inventario)
-
-# Agregar 1 unidad del producto con código 3 al carrito
-mi_carrito.agregar(3, 4, mi_inventario)
-# Quitar 1 unidad del producto con código 1 al carrito
-mi_carrito.quitar(1, 1, mi_inventario)
-# Listar todos los productos del carrito
-mi_carrito.mostrar()
-# Quitar 1 producto al carrito
-# Quitar 1 unidad del producto con código 1 al carrito
-mi_carrito.quitar(1, 1, mi_inventario)
-# Listar todos los productos del carrito
-mi_carrito.mostrar()
-# Mostramos el inventario
+# Listar los productos del inventario
 mi_inventario.listar_productos()
