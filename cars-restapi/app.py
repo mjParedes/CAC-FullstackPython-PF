4fc388a5ba8e289d38a90a057280ccd7d91cc8fc
Mauro Roth <maurogroth@gmail.com>
2023-07-01 18:45:26 -0300

    10. modificaciones en el app.py dentro de src

--- cars-restapi/app.py
+++ cars-restapi/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from .resources.cars import Cars
 
 app = Flask(__name__)
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    return jsonify({'message': 'pong!'})
+    pong = jsonify({'message': 'pong!'})
+    return render_template('index.html', mensaje=pong.json)
 
 @app.route('/cars', methods=['GET'])
 def getCars():
     return jsonify({'cars': Cars, 'message': "Cars List"})
+    #return render_template('carsList.html', carsList = carsList.json)
 
 @app.route('/cars/<string:car_name>', methods=['GET'])
 def getCar(car_name):
